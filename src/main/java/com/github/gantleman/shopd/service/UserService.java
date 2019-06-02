15ffcb0b956095da4020c44e809a3f780975e82f
gantleman <dds_sun@hotmail.com>
2019-06-02 17:40:47 +0800

    cccc

--- src/main/java/com/github/gantleman/shopd/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.github.gantleman.shopd.service;
-
-import java.util.List;
-import com.github.gantleman.shopd.entity.User;
-
-public interface UserService {
-    List<User> selectUsers();
-}
